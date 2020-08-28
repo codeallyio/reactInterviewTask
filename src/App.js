840e99afd1b7f817c87d28eef161d88fe573e113
adamzaczek <adamzaczekul@gmail.com>
2020-08-28 12:39:49 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React from 'react';
-import logo from './strove.jpg';
-import './App.css';
-import Task from './components/Task/Task'
+import React from "react";
+import Task from "./components/Task/Task";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://strove.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Strove
-        </a>
-      </header>
-      <Task />
-    </div>
-  );
+  return <Task />;
 }
 
 export default App;
